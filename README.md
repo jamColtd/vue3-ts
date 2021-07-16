06279bbd789110808a0d4030266835535d56300f
ex_min.dong <ex_min.dong@tcl.com>
2021-07-16 09:07:45 +0800

    0716 --- 时间模块

--- README.md
+++ README.md
@@ -356,6 +356,29 @@ body,html{
 </style>
 
 ````
+###### 事件模块
+
+`````javascript
+
+// 创建文件hooks
+// 里面放useNowTime.ts
+    import {ref} from 'vue';
+
+    const nowTime = ref("00:00:00")
+    const getNowTime = ()=>{ 
+        const now = new Date();
+        const hour = now.getHours()<10?"0"+now.getHours():now.getHours();
+        const minu = now.getMinutes()<10?"0"+now.getMinutes():now.getMinutes();
+        const sec = now.getSeconds()<10?"0"+now.getSeconds():now.getSeconds();
+        nowTime.value = hour+":"+minu+":"+sec;
+        setTimeout(getNowTime,1000)
+    }
+
+    export {nowTime, getNowTime}
+
+    // 2.在界面中引用过
+    import {nowTime, getNowTime} from './hooks/useNowTime'
+`````
 
 
 
--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld />
-  <h2>欢迎来到德莱联盟</h2>
-  <div>请选择一个英雄</div>
-  <div>
-    <button 
-      v-for="(item, index) in heros" 
-      :key="index"
-      @click="selectHeroFun(index)"
-    >
-      {{ index }}:{{ item }}</button>
-  </div>
-  <div>你选择的英雄是【{{ selectHero }}】</div>
-  <div><button @click="overAction">选择完毕</button></div>
-  <div>{{overText}}</div>
+  <div>{{nowTime}}</div>
+  <div><button @click="getNowTime">显示时间</button></div>
 </template>
 
 <script lang="ts">
-  import { 
-    defineComponent, 
-    ref, 
-    reactive, 
-    toRefs, 
-    onBeforeMount, 
-    onMounted,
-    onBeforeUpdate,
-    onUpdated,
-    onBeforeUnmount,//组件卸载之前
-    onUnmounted,
-    onRenderTracked,//vue3新增加的 状态 跟踪
-    onRenderTriggered,
-    watch,
-  } from 'vue';
-  import HelloWorld from './components/login.vue';
-
-  interface DataProps{
-    heros: string[];
-    selectHero: string;
-    selectHeroFun: (index: number) => void;
-  }
+  import {nowTime, getNowTime} from './hooks/useNowTime'
 
   export default({
     name: 'App',
     setup(){
-      console.log("1-开始创建组件-----setup()")
-      const data: DataProps = reactive({
-        heros:["刘备", "张飞", "关羽"],
-        selectHero:'',
-        selectHeroFun:(index:number)=>{
-          data.selectHero = data.heros[index]
-        }
-      });
-      const refData = toRefs(data);
       
-      const overText = ref("英雄联盟");
-      const overAction = () =>{
-        overText.value = '选择完毕|' + overText.value; 
-        //document.title = overText.value;
-      }
-
-      watch([overText, () => data.selectHero], (newValue, oldValue)=>{
-        console.log(`new---->${newValue}`);
-        console.log(`old---->${oldValue}`);
-
-        document.title = newValue[0];
-      })
-
-
       return{
-        ...refData,
-        overText,
-        overAction
+        nowTime,getNowTime
       }
     }
   });
